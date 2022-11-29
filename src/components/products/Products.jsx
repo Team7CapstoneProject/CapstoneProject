99a6699af53b170378f3092e4a6d903d78e783e7
Bella <Isabellapinkerman@gmail.com>
2022-11-29 12:05:30 -0500

    passed cart state through to ProductAddToCart, createCart in Main.jsx

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Product } from "../";
 
-const Products = ({ products }) => {
+const Products = ({ products, cart, setCart }) => {
   return (
     <>
       <div className="products">
         {products.length ? (
           products.map((product) => {
-            return <Product key={`product-${product.id}`} product={product} />;
+            return <Product key={`product-${product.id}`} product={product} cart={cart} setCart={setCart}/>;
           })
         ) : (
           <div>No Products Found</div>
