6ebb326633485afaa43c50a9a06687461f07ae38
Robby Bacus <robbybacus@gmail.com>
2022-11-28 22:54:35 -0500

    working product.jsx & product css

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,9 +1,53 @@
 import React from "react";
-const Product = ({product}) => {
+const Product = ({ product }) => {
+  
+  let salePrice
+  let finalSalePrice;
+
+  if (product.on_sale === true) {
+    let percentageConversion = product.sale_percentage * 0.01;
+    salePrice = product.price * (1 - percentageConversion);
+    //.toFixed rounds up the number to the nearest 2 decimal places. 
+    //finalSalePrice needs to be force coerced into a number since .toFixed returns a string.
+    finalSalePrice = Number(salePrice.toFixed(2))
+  } else {
+    finalSalePrice = product.price;
+  }
+
   return (
     <>
-      <div>{product.name}</div>
-      <div>{product.description}</div>
+      <div className="productCard">
+        <img
+          src={product.image_url}
+          alt={product.name}
+          className="productImage"
+        />
+
+        <div className="productInfo">
+          <div>{product.name}</div>
+          <div>Description: {product.description}</div>
+        </div>
+
+        <div>
+          {product.on_sale === true ? (
+            <div className="productSale">
+              <div className="productFinalPrice">{`$${finalSalePrice}`}</div>
+              <div className="saleCard">
+                <div className="salePercentage">{`${product.sale_percentage}% off!`}</div>
+                <div className="productInitialPrice">
+                  <strike>{`$${product.price}`}</strike>
+                </div>
+              </div>
+              <div className="productInventory">{`${product.inventory} item(s) left!`}</div>
+            </div>
+          ) : (
+            <div className="productNoSale">
+              <div className="productFinalPrice">{`$${finalSalePrice}`}</div>
+              <div className="productInventory">{`${product.inventory} item(s) left!`}</div>
+            </div>
+          )}
+        </div>
+      </div>
     </>
   );
 };
