50bb553f842dcba59de876053ccd9ee5e50e7d2a
Robby Bacus <robbybacus@gmail.com>
2022-11-30 00:40:25 -0500

    rearranged some adminProduct divs

--- src/components/admin/AdminProduct.jsx
+++ src/components/admin/AdminProduct.jsx
@@ -1,24 +1,33 @@
 import React from "react";
-import {EditProduct} from "../"
+import { EditProduct } from "../";
 
 const AdminProduct = ({ adminProduct }) => {
-    console.log("adminProduct data", adminProduct)
+  // console.log("adminProduct data", adminProduct);
 
-    
-    //component is receiving adminProduct data from AdminProducts component
-    //here we are mapping through each relevant part of the products data we receive to seperate divs
+  //component is receiving adminProduct data from AdminProducts component
+  //here we are mapping through each relevant part of the products data we receive to seperate divs
   return (
     <>
       <div>
-      <div>{`Product ID: ${adminProduct.id}`}</div>
+        <div>{`Product ID: ${adminProduct.id}`}</div>
         <div>{`Name: ${adminProduct.name}`}</div>
         <div>{`Description: ${adminProduct.description}`}</div>
         <div>{`Image URL: ${adminProduct.image_url}`}</div>
         <div>{`Inventory: ${adminProduct.inventory}`}</div>
         <div>{`Price: ${adminProduct.price}`}</div>
-        <div>{`Sale Status: ${adminProduct.on_sale}`}</div>
-        <div>{`Sale Discount Percentage: ${adminProduct.sale_percentage}%`}</div>
-        <EditProduct adminProduct={adminProduct}/>
+        <div>
+          {adminProduct.on_sale === true ? (
+            <div>
+              {" "}
+              <div>{`Sale Status: ${adminProduct.on_sale}`}</div>
+              <div>{`Sale Discount Percentage: ${adminProduct.sale_percentage}%`}</div>
+            </div>
+          ) : (
+            <div>{`Sale Status: ${adminProduct.on_sale}`}</div>
+          )}
+        </div>
+
+        <EditProduct adminProduct={adminProduct} />
       </div>
     </>
   );
