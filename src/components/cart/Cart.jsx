8d2ebe2b223866d9f2da4d0f64e2acfbb93abe22
hrbaldwin <haileyrebeccabaldwin@gmail.com>
2022-12-02 10:33:42 -0600

    a start on cart styling

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { deleteCartProduct, updateCartProductQuantity } from "../../api";
+import "./CSS/cart.css";
 import { Link, useParams } from "react-router-dom";
 
 const Cart = ({ cart, setCart }) => {
@@ -64,64 +65,74 @@ const Cart = ({ cart, setCart }) => {
   console.log(cart);
   return (
     <>
-      <h2>cart:</h2>
-      <div>
-        {cart.products ? (
-          cart.products.map((product) => {
-            // console.log(product, "THIS IS PRODUCT 35");
-            return (
-              <div key={`product-${product.id}`}>
-                <h3>{product.name}</h3>
-                <img
-                  className="productImage"
-                  src={product.image_url}
-                  alt={`${product.name} Image`}
-                />
-                <p>${product.price}</p>
-                <p>
-                  Quantity:
-                  {product.quantity}
-                  <button
-                    onClick={() => {
-                      decrement(product.id, product.quantity);
-                    }}
-                  >
-                    -
-                  </button>
-                  <button
-                    onClick={() => {
-                      increment(product.id, product.quantity);
-                    }}
-                  >
-                    +
-                  </button>
-                </p>
-                <button
-                  onClick={handleDelete}
-                  id={product.cartProductId ? `${product.cartProductId}` : null}
-                >
-                  delete
-                </button>
-                {product.on_sale == true ? (
-                  <p>{product.sale_percentage}% off</p>
-                ) : null}
-              </div>
-            );
-          })
-        ) : (
-          <div>
-            <Link to={"/products"}>
-              {" "}
-              <button>add products to cart!</button>{" "}
-            </Link>
-          </div>
-        )}
-      </div>
-      <div>
-        <div className="subtotal"></div>
-        <Link to={"/checkout"}>
-          <button>Checkout Here!</button>
-        </Link>
+      <h2 className="cartHeader">Your shopping cart</h2>
+      <div className="cartPageDiv">
+        <div className="cartProductDiv">
+          {cart.products ? (
+            cart.products.map((product) => {
+              // console.log(product, "THIS IS PRODUCT 35");
+              return (
+                <div key={`product-${product.id}`} className="cartProduct">
+                  <img
+                    className="productImage"
+                    src={product.image_url}
+                    alt={`${product.name} Image`}
+                  />
+                  <div>
+                    <h3>{product.name}</h3>
+                    <p>${product.price}</p>
+                    {product.on_sale == true ? (
+                      <p>{product.sale_percentage}% off</p>
+                    ) : null}
+                    <p>
+                      Quantity:
+                      {product.quantity}
+                      <button
+                        onClick={() => {
+                          decrement(product.id, product.quantity);
+                        }}
+                      >
+                        -
+                      </button>
+                      <button
+                        onClick={() => {
+                          increment(product.id, product.quantity);
+                        }}
+                      >
+                        +
+                      </button>
+                    </p>
+
+                    <p
+                      onClick={handleDelete}
+                      id={
+                        product.cartProductId
+                          ? `${product.cartProductId}`
+                          : null
+                      }
+                      className="remove"
+                    >
+                      Remove from cart
+                    </p>
+                  </div>
+                </div>
+              );
+            })
+          ) : (
+            <div>
+              <Link to={"/products"}>
+                {" "}
+                <button>add products to cart!</button>{" "}
+              </Link>
+            </div>
+          )}
+        </div>
+        <div className="checkoutDiv">
+          <div className="subtotal"> Total: $</div>
+          <Link to={"/checkout"}>
+            <button className="checkoutButton">Continue to checkout</button>
+          </Link>
+        </div>
       </div>
     </>
   );
