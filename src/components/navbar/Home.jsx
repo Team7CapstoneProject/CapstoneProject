3bc1f53f374e946012a9d388528a896b8e58ec35
hrbaldwin <haileyrebeccabaldwin@gmail.com>
2022-12-01 21:40:46 -0600

    current work on styling Home component

--- src/components/navbar/Home.jsx
+++ src/components/navbar/Home.jsx
@@ -2,7 +2,16 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <div>This is Home Component</div>
+      <img
+        src="https://i.imgur.com/Z4YeMD5.png"
+        alt="display of holiday sale items"
+        className="saleImage"
+      />
+      <h3 className="newIn">What's New</h3>
+      {/* display featured products */}
+      <div className="featProductDiv">
+        <p className="featProduct"> ... featured product display ... </p>
+      </div>
     </>
   );
 };
