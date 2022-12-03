71762bb2408a57cfa4c3e81cb31f8b1b7317a2f7
Bella <Isabellapinkerman@gmail.com>
2022-12-03 10:11:21 -0500

    basic styling for single product view

--- src/components/products/ViewOfProducts.jsx
+++ src/components/products/ViewOfProducts.jsx
@@ -4,29 +4,29 @@ import ProductAddToCart from "./ProductAddToCart";
 
 const ViewOfProducts = ({ allProducts }) => {
   let { productId } = useParams();
-  console.log(productId);
-  console.log(allProducts);
   return (
-    <div>
+    <div className="viewCard">
       {allProducts.map((product) => {
         if (productId == product.id) {
           return (
-            <div>
+            <div key={product.name} className="productView">
               <img
-                className="productImage"
+                className="productImageView"
                 src={product.image_url}
                 alt={`${product.name} Image`}
               />
-              <div className="productInfo">
-                <div>{product.name}</div>
+              <div className="productInfoView">
+                <div className="nameView">{product.name}</div>
                 <div>Description: {product.description}</div>
-              </div>
-              <div>${product.price}</div>
+                <div className="priceView">${product.price}</div>
+            </div>
             </div>
           );
         }
       })}
+      <div className="addView">
       <ProductAddToCart />
+    </div>
     </div>
   );
 };
