6a32d169174d83f35258312a2aece217d26be1f6
Robby Bacus <robbybacus@gmail.com>
2022-12-06 12:09:07 -0500

    working search function for products in admin dashboard

--- src/components/admin/AdminProductsSearch.jsx
+++ src/components/admin/AdminProductsSearch.jsx
@@ -1,45 +1,55 @@
-// import React, { useEffect, useState } from "react";
-// import { Products } from "../";
-// import "./CSS/products.css";
+import React, { useEffect, useState } from "react";
+import { AdminProducts } from "..";
 
-// const AdminProductsSearch = ({ allProducts, cart, setCart }) => {
-//   const [products, setProducts] = useState(allProducts);
+const AdminProductsSearch = ({ token, allProducts, setAllProducts }) => {
+  const [products, setProducts] = useState(allProducts);
 
-//   useEffect(() => {
-//     setProducts(allProducts);
-//   }, [allProducts]);
+  useEffect(() => {
+    setProducts(allProducts);
+  }, [allProducts]);
 
-//   function searchProducts(searchValue) {
-//     if (searchValue !== "") {
-//       const filteredProducts = allProducts.filter((product) => {
-//         return Object.values(product)
-//           .join("")
-//           .toLowerCase()
-//           .includes(searchValue.toLowerCase());
-//       });
-//       setProducts(filteredProducts);
-//     } else {
-//       setProducts(allProducts);
-//     }
-//   }
+  function searchProducts(searchValue) {
+    if (searchValue !== "") {
+      const filteredProducts = allProducts.filter((product) => {
+        return Object.values(product)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase());
+      });
+      setProducts(filteredProducts);
+    } else {
+      setProducts(allProducts);
+    }
+  }
 
-//   const handleChange = (input) => {
-//     input.preventDefault();
-//     searchProducts(input.target.value);
-//   };
+  const handleChange = (input) => {
+    input.preventDefault();
+    searchProducts(input.target.value);
+  };
 
-//   return (
-//     <>
-//       <div className="productPage">
-//         <div>
-//           <form className="productSearchForm">
-//             <input className="productSearchInput" type="text" placeholder="Search in GuitarStop" onChange={handleChange} autoFocus/>
-//           </form>
-//         </div>
-//         <Products products={products} cart={cart} setCart={setCart}/>
-//       </div>
-//     </>
-//   );
-// };
+  return (
+    <>
+      <div className="productPage">
+        <div>
+          <form className="productSearchForm">
+            <input
+              className="productSearchInput"
+              type="text"
+              placeholder="Search products"
+              onChange={handleChange}
+              autoFocus
+            />
+          </form>
+        </div>
+        <AdminProducts
+          token={token}
+          allProducts={allProducts}
+          setAllProducts={setAllProducts}
+          products={products}
+        />
+      </div>
+    </>
+  );
+};
 
-// export default AdminProductsSearch;
+export default AdminProductsSearch;
