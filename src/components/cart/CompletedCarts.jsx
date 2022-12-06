6da17ae7378e57eab816f92ae705e17e3dc6f3e9
Grant <grant.mc.foster@gmail.com>
2022-12-06 11:17:31 -0600

    finished mapping order history items and product to div elements, needs styling/tweaking

--- src/components/cart/CompletedCarts.jsx
+++ src/components/cart/CompletedCarts.jsx
@@ -1,50 +1,62 @@
 import React, { useEffect, useState } from "react";
-import {
-  getCartByUserId
-} from "../../api"
+import { getCartByUserId } from "../../api";
 
-const CompletedCarts = ({userAccount}) => {
-console.log(userAccount)
+const CompletedCarts = ({ userAccount }) => {
+  console.log(userAccount);
 
-const [userCartHistory, setUserCartHistory] = useState();
+  const [userCartHistory, setUserCartHistory] = useState();
 
-useEffect(() => {
-  let token = localStorage.getItem("token");
-  if (token) {
-    async function fetchAllUserCarts() {
-      let userCarts = await getCartByUserId(token);
-      console.log("user cart data", userCarts);
-      if (userCarts && userCarts.length) {
-        let completedUserCarts = userCarts.filter((cart) => cart.is_complete === true);
-        console.log("completed carts", completedUserCarts);
-        setUserCartHistory(completedUserCarts);
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+    if (token) {
+      async function fetchAllUserCarts() {
+        let userCarts = await getCartByUserId(token);
+        console.log("user cart data", userCarts);
+        if (userCarts && userCarts.length) {
+          let completedUserCarts = userCarts.filter(
+            (cart) => cart.is_complete === true
+          );
+          console.log("completed carts", completedUserCarts);
+          setUserCartHistory(completedUserCarts);
+        }
       }
+      fetchAllUserCarts();
     }
-    fetchAllUserCarts();
-  }
-}, [])
+  }, []);
 
-console.log("user cart history data!", userCartHistory)
+  console.log("user cart history data!", userCartHistory);
   return (
     <div id="orderHistoryContainer">
       <div>Order History</div>
       <div id="orderHistoryItems">
-        {
-          userCartHistory && userCartHistory.length ? (
-            userCartHistory.map((cart) => {
-              return(
-                <div id="userCompletedCarts" key={`Cart-${cart.id}`}>
-                  <div>Order: {cart.id}</div>
+        {userCartHistory && userCartHistory.length ? (
+          userCartHistory.map((cart) => {
+            return (
+              <div id="userCompletedCarts" key={`Cart-${cart.id}`}>
+                <div>Order: {cart.id}</div>
+                <div id="orderHistoryProducts">
+                  {
+                    cart.products && cart.products.length ? (
+                      cart.products.map((product) => {
+                       return (
+                        <div key={`product-${product.id}`}>
+                          <div>Name: {product.name}</div>
+                          <div>Price: {product.price}</div>
+                          <div>Quantity: {product.quantity}</div>
+                        </div>
+                        )
+                      })
+                    ) : null
+                  }
                 </div>
-              )
-            })
-          ) : (
-            <div>No previous order found</div>
-          )
-        }
+              </div>
+            );
+          })
+        ) : (
+          <div>No previous order found</div>
+        )}
       </div>
     </div>
-
   );
 };
 export default CompletedCarts;
