963748804f714091c709b5176d6e1df5e28e7d9d
Grant <grant.mc.foster@gmail.com>
2022-12-06 10:59:21 -0600

    finished fetch setup for order history, starting map of div elements to display on page

--- src/components/cart/CompletedCarts.jsx
+++ src/components/cart/CompletedCarts.jsx
@@ -6,23 +6,43 @@ import {
 const CompletedCarts = ({userAccount}) => {
 console.log(userAccount)
 
-const [userCartHistory, setUserCartHistory] = useState([]);
+const [userCartHistory, setUserCartHistory] = useState();
 
 useEffect(() => {
-  let token = localStorage.getItem("token")
+  let token = localStorage.getItem("token");
   if (token) {
     async function fetchAllUserCarts() {
-      let userCarts = await getCartByUserId(token)
-      console.log("user cart data", userCarts)
+      let userCarts = await getCartByUserId(token);
+      console.log("user cart data", userCarts);
+      if (userCarts && userCarts.length) {
+        let completedUserCarts = userCarts.filter((cart) => cart.is_complete === true);
+        console.log("completed carts", completedUserCarts);
+        setUserCartHistory(completedUserCarts);
+      }
     }
     fetchAllUserCarts();
   }
 }, [])
 
+console.log("user cart history data!", userCartHistory)
   return (
     <div id="orderHistoryContainer">
       <div>Order History</div>
-
+      <div id="orderHistoryItems">
+        {
+          userCartHistory && userCartHistory.length ? (
+            userCartHistory.map((cart) => {
+              return(
+                <div id="userCompletedCarts" key={`Cart-${cart.id}`}>
+                  <div>Order: {cart.id}</div>
+                </div>
+              )
+            })
+          ) : (
+            <div>No previous order found</div>
+          )
+        }
+      </div>
     </div>
 
   );
